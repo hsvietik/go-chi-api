589256e1306285f464329648f468978877e1d3fe
Hanna Svietik <hsvietik@gmail.com>
2023-08-21 23:56:05 +0100

    middleware func added

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module go-chi-api
 
 go 1.21.0
 
-require github.com/go-chi/chi/v5 v5.0.10 // indirect
+require github.com/go-chi/chi/v5 v5.0.10
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
@@ -12,10 +13,27 @@ func main() {
 	
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/api/v1/test/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
-	})
+
+	r.With(MyMiddleware).Get("/api/v1/test/{mydata}", MyHandler)
 
 	fmt.Println("Server running on port :8000")
 	http.ListenAndServe(":8000", r)
+}
+
+//handler function that creates a response containing whatever is passed in {mydata} 
+func MyHandler(w http.ResponseWriter, r *http.Request){
+	mydata := chi.URLParam(r, "mydata") 
+	w.Write([]byte(fmt.Sprintf("My data is %v", mydata)))
+}
+
+
+// HTTP middleware setting a value on the request context
+func MyMiddleware( next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	  mydata := chi.URLParam(r, "mydata")
+	  ctx := context.WithValue(r.Context(), "mydata", mydata)
+	  ctxData:=ctx.Value("mydata")
+	  fmt.Printf("My data in context is %v  \n", ctxData)
+  	  next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
