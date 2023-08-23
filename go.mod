5f2d0f3b8f0978da9091bd5344bc8e89af28ad78
Hanna Svietik <hsvietik@gmail.com>
2023-08-23 01:59:09 +0100

    func to respond to frontend added

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module go-chi-api
 go 1.21.0
 
 require github.com/go-chi/chi/v5 v5.0.10
+
+require github.com/go-chi/cors v1.2.1 
--- main.go
+++ main.go
@@ -2,22 +2,35 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
-	"go-chi-api/roman"
+	// "go-chi-api/roman"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func main() {
 	
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	CORS := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	})
+	r.Use(CORS.Handler)
+	r.Use(middleware.SetHeader("Content-Type", "application/json"))
 	
 	r.With(myMiddleware).Get("/api/v1/test/{mydata}", MyHandler)
-	r.Get("/api/v1/test/convert", roman.RomanToInt)
+	r.Get("/api/v1/test/convert/", romanToInt)
 
 	fmt.Println("Server running on port :8000")
 	http.ListenAndServe(":8000", r)
@@ -40,4 +53,20 @@ func myMiddleware( next http.Handler) http.Handler {
 	  fmt.Printf("My data in context is %v  \n", ctxData)
   	  next.ServeHTTP(w, r.WithContext(ctx))
 	})
+}
+func romanToInt(w http.ResponseWriter, r *http.Request){
+	// result:= 0
+	romanInt := r.URL.Query().Get("query")
+	fmt.Println("Roman function")
+	fmt.Printf("My Roman int is %v", romanInt)
+
+	jsonData, err := json.Marshal(romanInt)
+	if err != nil {
+		log.Println(err.Error())
+		status := http.StatusInternalServerError
+		http.Error(w, http.StatusText(status), status)
+		return
+	}
+	w.Write(jsonData) 
+		
 }
